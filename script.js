e4d2f02aacf0c560a6790a3cd210e14b354d2e5d
subhanzakizada <zakizadasubhan@gmail.com>
2020-12-03 17:42:48 -0800

    add request loan

--- script.js
+++ script.js
@@ -186,3 +186,16 @@ btnClose.addEventListener('click', function(e) {
     }
     
 })
+
+
+btnLoan.addEventListener('click', function(e) {
+    e.preventDefault()
+    const amount = Number(inputLoanAmount.value)
+    
+    if(amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+        currentAccount.movements.push(amount)
+        updateUI(currentAccount)
+    
+       }
+    
+})
